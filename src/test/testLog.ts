e9ebfa0a79721cacc3102c8ffd9db9c30a2767fa
david@DAVID-PC <david@davidjenni.com>
2020-09-19 16:39:31 -0700

    clean up console logging, tests log into files

--- src/test/testLog.ts
+++ src/test/testLog.ts
@@ -1,18 +1,48 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 import { Logger } from "../lib";
-import { error, info, warn } from 'fancy-log';
+import { createLogger, format, transports } from 'winston';
+import path = require("path");
 
 export class TestLog implements Logger {
+    private _showTestLogs = process.env.DEBUG ?? false;
+    private _logger;
+
+    public constructor(logFile: string) {
+        logFile = path.resolve(__dirname, '..', '..', 'out', 'logs', logFile);
+
+        this._logger = createLogger({
+            level: 'info',
+            format: format.combine(
+                format.timestamp({
+                    format: 'YYYY-MM-DD HH:mm:ss'
+                }),
+                format.prettyPrint()
+            ),
+            transports: [
+                new transports.File({ filename: logFile })
+            ]
+        });
+
+        if (this._showTestLogs) {
+            this._logger.add(new transports.Console({
+                format: format.combine(
+                    format.colorize(),
+                    format.simple()
+                )
+            }));
+        }
+    }
+
     info(...args: string[]): void {
-        info(args);
+        this._logger.log('info', args);
     }
 
     warn(...args: string[]): void {
-        warn(args);
+        this._logger.log('warn', args);
     }
 
     error(...args: string[]): void {
-        error(args);
+        this._logger.log('error', args);
     }
 }
