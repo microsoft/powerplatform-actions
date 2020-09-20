dbf9e67d3667e5810cba2bdfe7b79c0ca000f8c5
david@DAVID-PC <david@davidjenni.com>
2020-09-20 11:15:11 -0700

    git test: assert for any commit

--- src/test/gitRunner.test.ts
+++ src/test/gitRunner.test.ts
@@ -7,13 +7,13 @@ import { GitRunner } from '../lib';
 import { TestLog } from './testLog';
 
 
-describe('SoPa', () => {
+describe('git', () => {
     const workDir = path.resolve(__dirname, '..', '..', 'out', 'test');
     const git = new GitRunner(workDir, new TestLog('git-tests.log'));
 
     it('can launch git log', async() => {
         const logs = await git.run(['log']);
         const firstCommit = logs.pop()?.trimStart();
-        expect(firstCommit).to.match(/^commit 4bd52fc/);
+        expect(firstCommit).to.match(/commit\s+[0-9a-z]{7,}/);
     });
 });
