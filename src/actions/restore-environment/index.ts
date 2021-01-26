465ec9b6d3336a846e5f7b3a5620e035e2f82331
Deepak Khobragade <deepakkhobragade4@gmail.com>
2021-01-26 10:02:30 -0800

    Exposed admin restore as github action (#49)
    * Exposed admin restore as github action
    
    * Updated the /dist folder for restore

--- src/actions/restore-environment/index.ts
+++ src/actions/restore-environment/index.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 import * as core from '@actions/core';
-import { DefaultRunnerFactory, RunnerFactory } from '../../lib';
+import { DefaultRunnerFactory, RunnerFactory} from '../../lib';
 
 (async () => {
     if (process.env.GITHUB_ACTIONS) {
@@ -11,32 +11,24 @@ import { DefaultRunnerFactory, RunnerFactory } from '../../lib';
 
 export async function main(factory: RunnerFactory): Promise<void> {
     try {
-        core.startGroup('create-environment:');
+        core.startGroup('restore-environment:');
+        const sourceUrl = core.getInput('source-url', { required: true});
+        const targetUrl = core.getInput('target-url', { required: true});
+        const selectedBackup = core.getInput('selected-backup', { required: true});
         const username = core.getInput('user-name', { required: true });
+
         const password = core.getInput('password-secret', { required: true });
         if (!password || password.length === 0) {
             return core.setFailed('Missing password! Specify one by setting input: \'password-secret\'');
         }
 
-        const envName = core.getInput('name', { required: true});
-        const envRegion = core.getInput('region', {required: true});
-        const envType = core.getInput('type', {required: true});
-        const domain = core.getInput('domain', {required: false});
-
         const pac = factory.getRunner('pac', process.cwd());
         await pac.run(['auth', 'clear']);
         await pac.run(['auth', 'create', '--kind', 'ADMIN', '--username', username, '--password', password]);
 
-        const createEnvironmentArgs = ['admin', 'create', '--name', envName, '--region', envRegion, '--type', envType, '--domain', domain];
-        const result = await pac.run(createEnvironmentArgs);
-        // HACK TODO: Need structured output from pac CLI to make parsing out of the resulting env URL more robust
-        const envUrl = result
-            .filter(l => l.length > 0)
-            .pop()
-            ?.trim()
-            .split(/\s+/)
-            .shift();
-        core.setOutput('environment-url', envUrl);
+        const restoreEnvArgs = ['admin', 'restore', '--source-url', sourceUrl, '--target-url', targetUrl, '--selected-backup', selectedBackup];
+        await pac.run(restoreEnvArgs);
+        core.info('environment restored');
         core.endGroup();
     } catch (error) {
         core.setFailed(`failed: ${error.message}`);
---
+++ src/test/restoreEnvironment.test.ts
@@ -0,0 +1,53 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+import path = require('path');
+import { forEachOf } from 'async';
+import { expect } from 'chai';
+
+import { main as restoreEnvironment } from '../actions/restore-environment';
+import { MockedRunners } from './mockedRunners';
+import { ActionInputsEmulator } from './actionInputsEmulator';
+
+describe('restore-environment#input validation', () => {
+    const workDir = path.resolve(__dirname, '..', '..', 'out', 'test');
+    const mockFactory: MockedRunners = new MockedRunners(workDir);
+    // TODO: read in params and their required state from the action.yml
+    const inputParams = [
+        { Name: 'user-name', Value: 'aUserName', required: true},
+        { Name: 'password-secret', Value: 'aSecret', required: true},
+        { Name: 'source-url', Value: 'sourceUrl', required: true},
+        { Name: 'target-url', Value: 'targetUrl', required: true},
+        { Name: 'selected-backup', Value: 'latest', required: true},
+    ];
+    const actionInputs = new ActionInputsEmulator(inputParams);
+
+    forEachOf(inputParams, (inputParam) => {
+        it(`required parameter - ${inputParam.Name}`, async() => {
+            actionInputs.defineInputsExcept(inputParam.Name);
+            let res, err;
+            try {
+                 res = await restoreEnvironment(mockFactory);
+            }
+            catch (error) {
+                err = error;
+            }
+            expect(res).to.be.undefined;
+            expect(err.message).to.match(new RegExp(`required and not supplied: ${inputParam.Name}`));
+        });
+    });
+
+    it('call action', async() => {
+        actionInputs.defineInputs();
+        let err;
+        try {
+            await restoreEnvironment(mockFactory);
+        }
+        catch (error) {
+            err = error;
+        }
+        expect(err).to.be.undefined;
+        const loggedCommands = mockFactory.loggedCommands;
+        expect(loggedCommands).to.deep.include({ RunnerName: 'pac', Arguments: [ 'auth', 'create', '--kind', 'ADMIN', '--username', 'aUserName', '--password', 'aSecret'] });
+        expect(loggedCommands).to.deep.include({ RunnerName: 'pac', Arguments: [ 'admin', 'restore', '--source-url', 'sourceUrl', '--target-url', 'targetUrl', '--selected-backup', 'latest'] });
+    }).timeout(5000);
+});
