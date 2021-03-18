6779fdde7ae168555c64199a9df9c833e67a3a7b
Crash Collison <Crash.Collison@microsoft.com>
2021-03-18 14:58:29 -0700

    Update gitRunner tests to work on Linux as well; revert debugging code

--- src/test/gitRunner.test.ts
+++ src/test/gitRunner.test.ts
@@ -13,17 +13,8 @@ describe('git', () => {
     const git = new GitRunner(workDir, logger);
 
     it('can launch git log', async() => {
-        const logs = await git.run(['log']);
-
-        // DEBUGGING - appears to be a difference in Git output between Windows and Linux
-        // revert prior to merge to main
-        //const firstCommit = logs.pop()?.trimStart();
-        //expect(firstCommit).to.match(/commit\s+[0-9a-z]{7,}/);
-        logger.info("DEBUGGING: Git logs array: ", ...logs);
-        const pop = logs.pop();
-        logger.info("DEBUGGING: pop: ", pop ? pop : "empty");
-        const trimmed = pop?.trimStart();
-        logger.info("DEBUGGING:  trimmed: ", trimmed ? trimmed : "empty");
-        expect(trimmed).to.match(/commit\s+[0-9a-z]{7,}/);
+        const logs = await git.run(['log', '-1']);
+        const line = logs.find(item => item && item.startsWith('commit'));
+        expect(line).to.match(/commit\s+[0-9a-z]{7,}/);
     });
 });
