f452fec00cf80ace880f6793c4d4c97c47aa8f07
Crash Collison <Crash.Collison@microsoft.com>
2021-03-18 14:38:57 -0700

    Debugging Git output difference between Win and Linux; Revert prior to merge to main

--- src/test/gitRunner.test.ts
+++ src/test/gitRunner.test.ts
@@ -9,11 +9,21 @@ import { TestLog } from './testLog';
 
 describe('git', () => {
     const workDir = path.resolve(__dirname, '..', '..', 'out', 'test');
-    const git = new GitRunner(workDir, new TestLog('git-tests.log'));
+    const logger = new TestLog('git-tests.log');
+    const git = new GitRunner(workDir, logger);
 
     it('can launch git log', async() => {
         const logs = await git.run(['log']);
-        const firstCommit = logs.pop()?.trimStart();
-        expect(firstCommit).to.match(/commit\s+[0-9a-z]{7,}/);
+
+        // DEBUGGING - appears to be a difference in Git output between Windows and Linux
+        // revert prior to merge to main
+        //const firstCommit = logs.pop()?.trimStart();
+        //expect(firstCommit).to.match(/commit\s+[0-9a-z]{7,}/);
+        logger.info("DEBUGGING: Git logs array: ", ...logs);
+        const pop = logs.pop();
+        logger.info("DEBUGGING: pop: ", pop ? pop : "empty");
+        const trimmed = pop?.trimStart();
+        logger.info("DEBUGGING:  trimmed: ", trimmed ? trimmed : "empty");
+        expect(trimmed).to.match(/commit\s+[0-9a-z]{7,}/);
     });
 });
