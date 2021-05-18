896ebf9a68c1773a2e28f42983f45a860e061e96
Deepak Khobragade <deepakkhobragade4@gmail.com>
2021-05-18 10:58:05 -0700

    Updated the pac cli to 1.6.6-daily-21051723

--- gulpfile.js
+++ gulpfile.js
@@ -178,9 +178,9 @@ async function setExecuteFlag(path) {
 
 const recompile = gulp.series(
     clean,
-    async () => nugetInstall('CAP_ISVExp_Tools_Daily', 'Microsoft.PowerApps.CLI.Core.linux-x64', '1.6.6-daily-21051123', path.resolve(outdir, 'pac_linux')),
+    async () => nugetInstall('CAP_ISVExp_Tools_Daily', 'Microsoft.PowerApps.CLI.Core.linux-x64', '1.6.6-daily-21051723', path.resolve(outdir, 'pac_linux')),
     async () => setExecuteFlag(path.resolve(outdir, 'pac_linux', 'tools', 'pac')),
-    async () => nugetInstall('CAP_ISVExp_Tools_Daily', 'Microsoft.PowerApps.CLI', '1.6.6-daily-21051123', path.resolve(outdir, 'pac')),
+    async () => nugetInstall('CAP_ISVExp_Tools_Daily', 'Microsoft.PowerApps.CLI', '1.6.6-daily-21051723', path.resolve(outdir, 'pac')),
     async () => nugetInstall('nuget.org', 'Microsoft.CrmSdk.CoreTools', '9.1.0.79', path.resolve(outdir, 'sopa')),
     compile
 );
