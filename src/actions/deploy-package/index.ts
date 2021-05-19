223d05c998d4fee0b57fb7f12c3d5967dd2f84a0
Deepak Khobragade <deepakkhobragade4@gmail.com>
2021-05-18 17:05:12 -0700

    Resolved PR comments; Testing logFile param log-file

--- src/actions/deploy-package/index.ts
+++ src/actions/deploy-package/index.ts
@@ -3,6 +3,7 @@
 import * as core from '@actions/core';
 import os = require('os');
 import { AuthKind, AuthHandler, DefaultRunnerFactory, RunnerFactory} from '../../lib';
+import * as artifact from '@actions/artifact';
 
 (async () => {
     if (process.env.GITHUB_ACTIONS) {
@@ -15,19 +16,25 @@ export async function main(factory: RunnerFactory): Promise<void> {
         core.startGroup('deploy-package:');
         const platform = os.platform();
         if (platform !== 'win32') {
-            throw Error(`Unsupported runner os: '${platform}'; package deployer is only available on Windows`);
+            throw Error(`Unsupported runner os: '${platform}'; package deployer is only available on Windows.`);
         }
         const pac = factory.getRunner('pac', process.cwd());
         const packagePath = core.getInput('package', { required: true });
-        const logToConsole = core.getInput('log-console', { required: false });
-        const LogToFile = core.getInput('log-file', { required: false });
-        core.info('deploying package with file : ' + packagePath);
-        console.log('deploying package with file : ' + packagePath)
-
+        const logToConsole = 'true';
+        const LogToFile = 'pac-deploy-log.txt';
         await new AuthHandler(pac).authenticate(AuthKind.CDS);
 
         const deployPackageArgs = ['package', 'deploy', '--package', packagePath, '--logFile', LogToFile, '--logConsole', logToConsole];
         await pac.run(deployPackageArgs);
+
+        const artifactClient = artifact.create();
+        const artifactName = 'pac-deploy-log';
+        const files = [
+            'pac-deploy-log.txt'
+        ]
+        const rootDirectory = '.';
+        const options = { continueOnError: true };
+        await artifactClient.uploadArtifact(artifactName, files, rootDirectory, options);
         core.info('package deployed.');
         core.endGroup();
     } catch (error) {
