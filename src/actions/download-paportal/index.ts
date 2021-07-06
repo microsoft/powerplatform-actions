bb4e4db976127363f1bc3ca420ba77c261fc0e5b
Ashish Choudhary <aschoud@microsoft.com>
2021-07-06 13:05:28 +0530

    implemented github action for paportal download

---
+++ src/actions/download-paportal/index.ts
@@ -0,0 +1,32 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+import * as core from '@actions/core';
+import { ActionLogger, AuthHandler, AuthKind, getWorkingDirectory, PacRunner } from '../../lib';
+
+
+core.startGroup('download-paportal:');
+const downloadPath = core.getInput('download-path', { required: true });
+const websiteId = core.getInput('website-id', { required: true });
+core.info(`downloadPath: path:${downloadPath} websiteId: ${websiteId} `);
+
+const workingDir = getWorkingDirectory('working-directory', false);
+
+const logger = new ActionLogger();
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+(async () => {
+    const pac = new PacRunner(workingDir, logger);
+    await new AuthHandler(pac).authenticate(AuthKind.CDS);
+
+    const exportArgs = ['paportal', 'download', '--path', downloadPath];
+
+    { exportArgs.push('--websiteId', websiteId); }
+
+    await pac.run(exportArgs);
+    core.info(`downloading portal data for websiteId: ${websiteId} at location : ${downloadPath}`);
+    core.endGroup();
+
+})().catch(error => {
+    core.setFailed(`failed: ${error}`);
+    core.endGroup();
+});
