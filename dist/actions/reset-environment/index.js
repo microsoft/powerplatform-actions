89a028edb909da1ff9f16993a98ce8a215dd837e
david@DAVID-PC <david@davidjenni.com>
2021-08-12 14:10:07 -0700

    update dist/ folder

--- dist/actions/reset-environment/index.js
+++ dist/actions/reset-environment/index.js
@@ -968,7 +968,7 @@ exports.GitRunner = GitRunner;
 
 
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.AuthKind = exports.AuthHandler = exports.SopaRunner = exports.PacRunner = exports.GitRunner = exports.ActionLogger = exports.DefaultRunnerFactory = exports.RunnerError = exports.getWorkingDirectory = exports.getInputAsBool = void 0;
+exports.AuthKind = exports.AuthHandler = exports.PacRunner = exports.GitRunner = exports.ActionLogger = exports.DefaultRunnerFactory = exports.RunnerError = exports.getWorkingDirectory = exports.getInputAsBool = void 0;
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 var actionInput_1 = __nccwpck_require__(434);
@@ -985,8 +985,6 @@ var gitRunner_1 = __nccwpck_require__(973);
 Object.defineProperty(exports, "GitRunner", ({ enumerable: true, get: function () { return gitRunner_1.GitRunner; } }));
 var pacRunner_1 = __nccwpck_require__(366);
 Object.defineProperty(exports, "PacRunner", ({ enumerable: true, get: function () { return pacRunner_1.PacRunner; } }));
-var sopaRunner_1 = __nccwpck_require__(653);
-Object.defineProperty(exports, "SopaRunner", ({ enumerable: true, get: function () { return sopaRunner_1.SopaRunner; } }));
 var authHandler_1 = __nccwpck_require__(677);
 Object.defineProperty(exports, "AuthHandler", ({ enumerable: true, get: function () { return authHandler_1.AuthHandler; } }));
 Object.defineProperty(exports, "AuthKind", ({ enumerable: true, get: function () { return authHandler_1.AuthKind; } }));
@@ -1032,7 +1030,6 @@ exports.DefaultRunnerFactory = void 0;
 const actionLogger_1 = __nccwpck_require__(970);
 const gitRunner_1 = __nccwpck_require__(973);
 const pacRunner_1 = __nccwpck_require__(366);
-const sopaRunner_1 = __nccwpck_require__(653);
 class RealRunnerFactory {
     constructor() {
         this._logger = new actionLogger_1.ActionLogger();
@@ -1043,8 +1040,6 @@ class RealRunnerFactory {
                 return new pacRunner_1.PacRunner(workingDir, this._logger);
             case 'git':
                 return new gitRunner_1.GitRunner(workingDir, this._logger);
-            case 'sopa':
-                return new sopaRunner_1.SopaRunner(workingDir, this._logger);
             default:
                 throw new Error(`Unknown runner type requested: ${runnerName}`);
         }
@@ -1082,32 +1077,6 @@ const runnerParameters = {
 exports.runnerParameters = runnerParameters;
 
 //# sourceMappingURL=runnerParameters.js.map
-
-
-/***/ }),
-
-/***/ 653:
-/***/ ((__unused_webpack_module, exports, __nccwpck_require__) => {
-
-
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.SopaRunner = void 0;
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-const exeRunner_1 = __nccwpck_require__(21);
-const os = __nccwpck_require__(87);
-class SopaRunner extends exeRunner_1.ExeRunner {
-    constructor(workingDir, logger) {
-        super(workingDir, logger, 'SolutionPackager.exe', ['sopa', 'content', 'bin', 'coretools']);
-        const platform = os.platform();
-        if (platform !== 'win32') {
-            throw Error(`Unsupported SoPa runner os: '${platform}'; the standalone SoPa executable is only available on Windows`);
-        }
-    }
-}
-exports.SopaRunner = SopaRunner;
-
-//# sourceMappingURL=sopaRunner.js.map
 
 
 /***/ }),
