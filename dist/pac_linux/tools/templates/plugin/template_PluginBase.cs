fd56128486da41a5432c8f906c8fdbbb5341bac2
david@DAVID-PC <davidjen@microsoft.com>
2021-09-21 20:10:03 -0700

    update binaries in dist/

--- dist/pac_linux/tools/templates/plugin/template_PluginBase.cs
+++ dist/pac_linux/tools/templates/plugin/template_PluginBase.cs
@@ -4,19 +4,31 @@ using System.ServiceModel;
 
 namespace $safeprojectname$
 {
-    /* 
-     * Plugin development guide: https://docs.microsoft.com/powerapps/developer/common-data-service/plug-ins
-     * Best practices and guidance: https://docs.microsoft.com/powerapps/developer/common-data-service/best-practices/business-logic/
-     */
+    /// <summary>
+    /// Base class for all plug-in classes.
+    /// Plugin development guide: https://docs.microsoft.com/powerapps/developer/common-data-service/plug-ins
+    /// Best practices and guidance: https://docs.microsoft.com/powerapps/developer/common-data-service/best-practices/business-logic/
+    /// </summary>    
     public abstract class PluginBase : IPlugin
     {
         protected string PluginClassName { get; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PluginBase"/> class.
+        /// </summary>
+        /// <param name="pluginClassName">The <see cref=" cred="Type"/> of the plugin class.</param>
         internal PluginBase(Type pluginClassName)
         {
             PluginClassName = pluginClassName.ToString();
         }
 
+        /// <summary>
+        /// Main entry point for he business logic that the plug-in is to execute.
+        /// </summary>
+        /// <param name="serviceProvider">The service provider.</param>
+        /// <remarks>
+        /// </remarks>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1303:Do not pass literals as localized parameters", Justification = "Execute")]
         public void Execute(IServiceProvider serviceProvider)
         {
             if (serviceProvider == null)
@@ -24,14 +36,16 @@ namespace $safeprojectname$
                 throw new InvalidPluginExecutionException("serviceProvider");
             }
 
+            // Construct the local plug-in context.
             var localPluginContext = new LocalPluginContext(serviceProvider);
 
             localPluginContext.Trace($"Entered {PluginClassName}.Execute() " +
-                $"Correlation Id: {localPluginContext.PluginExecutionContext.CorrelationId}, " +
-                $"Initiating User: {localPluginContext.PluginExecutionContext.InitiatingUserId}");
+                 $"Correlation Id: {localPluginContext.PluginExecutionContext.CorrelationId}, " +
+                 $"Initiating User: {localPluginContext.PluginExecutionContext.InitiatingUserId}");
 
             try
             {
+                // Invoke the custom implementation 
                 ExecuteCdsPlugin(localPluginContext);
 
                 // Now exit - if the derived plugin has incorrectly registered overlapping event registrations, guard against multiple executions.
@@ -49,21 +63,26 @@ namespace $safeprojectname$
             }
         }
 
+        /// <summary>
+        /// Placeholder for a custom plug-in implementation. 
+        /// </summary>
+        /// <param name="localPluginContext">Context for the current plug-in.</param>
         protected virtual void ExecuteCdsPlugin(ILocalPluginContext localPluginContext)
         {
             // Do nothing. 
         }
+
     }
 
-     /*
-     * This interface provides an abstraction on top of IServiceProvider for commonly used PowerApps CDS Plugin development constructs
-     */
+    /// <summary>
+    /// This interface provides an abstraction on top of IServiceProvider for commonly used PowerPlatform Dataverse Plugin development constructs
+    /// </summary>
     public interface ILocalPluginContext
     {
-        // The PowerApps CDS organization service for current user account
+        // The PowerPlatform Dataverse organization service for current user account
         IOrganizationService CurrentUserService { get; }
 
-        // The PowerApps CDS organization service for system user account
+        // The PowerPlatform Dataverse organization service for system user account
         IOrganizationService SystemUserService { get; }
 
         // IPluginExecutionContext contains information that describes the run-time environment in which the plugin executes, information related to the execution pipeline, and entity business information
@@ -76,22 +95,47 @@ namespace $safeprojectname$
         // Provides logging run time trace information for plug-ins. 
         ITracingService TracingService { get; }
 
-        // Writes a trace message to the CDS trace log
+        // Writes a trace message to the Dataverse trace log
         void Trace(string message);
     }
 
+    /// <summary>
+    /// Plug-in context object. 
+    /// </summary>
     public class LocalPluginContext : ILocalPluginContext
     {
+        internal IServiceProvider ServiceProvider { get; }
+
+        /// <summary>
+        /// The PowerPlatform Dataverse organization service for current user account.
+        /// </summary>
         public IOrganizationService CurrentUserService { get; }
 
+        /// <summary>
+        /// The PowerPlatform Dataverse organization service for system user account.
+        /// </summary>
         public IOrganizationService SystemUserService { get; }
 
+        /// <summary>
+        /// IPluginExecutionContext contains information that describes the run-time environment in which the plug-in executes, information related to the execution pipeline, and entity business information.
+        /// </summary>
         public IPluginExecutionContext PluginExecutionContext { get; }
 
+        /// <summary>
+        /// Synchronous registered plug-ins can post the execution context to the Microsoft Azure Service Bus. <br/> 
+        /// It is through this notification service that synchronous plug-ins can send brokered messages to the Microsoft Azure Service Bus.
+        /// </summary>
         public IServiceEndpointNotificationService NotificationService { get; }
 
+        /// <summary>
+        /// Provides logging run-time trace information for plug-ins. 
+        /// </summary>
         public ITracingService TracingService { get; }
 
+        /// <summary>
+        /// Helper object that stores the services available in this plug-in.
+        /// </summary>
+        /// <param name="serviceProvider"></param>
         public LocalPluginContext(IServiceProvider serviceProvider)
         {
             if (serviceProvider == null)
@@ -112,6 +156,10 @@ namespace $safeprojectname$
             SystemUserService = factory.CreateOrganizationService(null);
         }
 
+        /// <summary>
+        /// Writes a trace message to the CRM trace log.
+        /// </summary>
+        /// <param name="message">Message name to trace.</param>
         public void Trace(string message)
         {
             if (string.IsNullOrWhiteSpace(message) || TracingService == null)
@@ -119,13 +167,24 @@ namespace $safeprojectname$
                 return;
             }
 
-            TracingService.Trace(message);
+            if (PluginExecutionContext == null)
+            {
+                TracingService.Trace(message);
+            }
+            else
+            {
+                TracingService.Trace(
+                    "{0}, Correlation Id: {1}, Initiating User: {2}",
+                    message,
+                    PluginExecutionContext.CorrelationId,
+                    PluginExecutionContext.InitiatingUserId);
+            }
         }
     }
 
-    /*
-     * Specialized ITracingService implementation that prefixes all traced messages with a time delta for Plugin performance diagnostics
-     */
+    /// <summary>
+    /// Specialized ITracingService implementation that prefixes all traced messages with a time delta for Plugin performance diagnostics
+    /// </summary>
     public class LocalTracingService : ITracingService
     {
         private readonly ITracingService _tracingService;
