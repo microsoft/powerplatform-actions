2a8fcccc47dace4b9b12a6767fd898e2040a7605
penguy-ms <71346301+penguy-ms@users.noreply.github.com>
2021-09-28 13:30:12 -0700

    Whoami using updated cli wrapper and adding yaml parser (#120)
    * Whoami using updated cli wrapper and adding yaml parser
    
    * Add unit test; cast json to any
    
    * Bump cli-wrapper

---
+++ src/lib/parser/YamlParser.ts
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { HostParameterEntry } from "@microsoft/powerplatform-cli-wrapper/dist/host/IHostAbstractions";
+import fs = require('fs');
+import yaml = require('js-yaml');
+import path = require("path");
+
+export class YamlParser {
+    public getHostParameterEntries(workingDir: string, actionFolder: string): Record<string, HostParameterEntry> {
+        const parameterMap: Record<string, HostParameterEntry> = {};
+        try {
+            const file = path.resolve(workingDir, `./${actionFolder}/action.yml`);
+            const fileContents = fs.readFileSync(file, 'utf8');
+            const data = yaml.load(fileContents) as any;
+            for (const [name, inputInfo] of Object.entries(data.inputs)) {
+                const typedData = {
+                    name: name,
+                    required: (inputInfo as any).required ?? false,
+                    defaultValue: (inputInfo as any).default,
+                }
+                parameterMap[name] = typedData;
+            }
+            return parameterMap;
+        } catch (e) {
+            throw new Error(`Error parsing yaml file for ${actionFolder}`)
+        }
+    }
+}
