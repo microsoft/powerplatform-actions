c70d21ea6094519101c96b77a33ed9f87f6d8aea
Peterson Nguyen <penguy@microsoft.com>
2021-11-04 14:18:28 -0700

    Checking for bug

--- src/test/deployPackage.test.ts
+++ src/test/deployPackage.test.ts
@@ -10,6 +10,7 @@ import { UsernamePassword } from "@microsoft/powerplatform-cli-wrapper";
 import { runnerParameters } from "../../src/lib/runnerParameters";
 import Sinon = require("sinon");
 import { ActionsHost } from "../lib/host/ActionsHost";
+import { assert } from "console";
 should();
 use(sinonChai);
 
@@ -20,7 +21,7 @@ describe("deploy package test", () => {
   const environmentUrl = "environment url";
 
   async function callActionWithMocks(): Promise<void> {
-    const deployPackage = await rewiremock.around(
+    await rewiremock.around(
       () => import("../../src/actions/deploy-package/index"),
       (mock) => {
         mock(() => import("@microsoft/powerplatform-cli-wrapper/dist/actions")).with({ deployPackage: deployPackageStub });
@@ -29,17 +30,20 @@ describe("deploy package test", () => {
         mock(() => import("fs/promises")).with({ chmod: fake() });
         mock(() => import("../../src/lib/runnerParameters")).with({ runnerParameters: runnerParameters });
       });
-    await deployPackage.main();
   }
 
   it("calls deploy package", async () => {
 
     await callActionWithMocks();
 
-    deployPackageStub.should.have.been.calledWithExactly({
+    deployPackageStub.should.have.been.calledOnceWithExactly({
       credentials: credentials,
       environmentUrl: environmentUrl,
       packagePath: { name: 'package', required: true, defaultValue: undefined },
     }, runnerParameters, new ActionsHost());
+  });
+
+  it("undefined checker", async () => {
+    chai.assert.isNotNull(process.env.GITHUB_ACTIONS);
   });
 });
