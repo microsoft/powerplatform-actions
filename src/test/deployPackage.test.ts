10ef9c4c019149b0c39612cf29116470fbf7958b
penguy-ms <71346301+penguy-ms@users.noreply.github.com>
2021-11-04 13:17:27 -0700

    Updating deploy package to use cli wrapper (#135)

---
+++ src/test/deployPackage.test.ts
@@ -0,0 +1,45 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { should, use } from "chai";
+import { stubInterface } from "ts-sinon";
+import * as sinonChai from "sinon-chai";
+import rewiremock from "./rewiremock";
+import { fake, stub } from "sinon";
+import { UsernamePassword } from "@microsoft/powerplatform-cli-wrapper";
+import { runnerParameters } from "../../src/lib/runnerParameters";
+import Sinon = require("sinon");
+import { ActionsHost } from "../lib/host/ActionsHost";
+should();
+use(sinonChai);
+
+describe("deploy package test", () => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const deployPackageStub: Sinon.SinonStub<any[], any> = stub();
+  const credentials: UsernamePassword = stubInterface<UsernamePassword>();
+  const environmentUrl = "environment url";
+
+  async function callActionWithMocks(): Promise<void> {
+    const deployPackage = await rewiremock.around(
+      () => import("../../src/actions/deploy-package/index"),
+      (mock) => {
+        mock(() => import("@microsoft/powerplatform-cli-wrapper/dist/actions")).with({ deployPackage: deployPackageStub });
+        mock(() => import("../../src/lib/auth/getCredentials")).withDefault(() => credentials );
+        mock(() => import("../../src/lib/auth/getEnvironmentUrl")).withDefault(() => environmentUrl );
+        mock(() => import("fs/promises")).with({ chmod: fake() });
+        mock(() => import("../../src/lib/runnerParameters")).with({ runnerParameters: runnerParameters });
+      });
+    await deployPackage.main();
+  }
+
+  it("calls deploy package", async () => {
+
+    await callActionWithMocks();
+
+    deployPackageStub.should.have.been.calledWithExactly({
+      credentials: credentials,
+      environmentUrl: environmentUrl,
+      packagePath: { name: 'package', required: true, defaultValue: undefined },
+    }, runnerParameters, new ActionsHost());
+  });
+});
