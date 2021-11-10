03b5ad1684db92c4c66393d622c10c1335a79ff0
Peterson Nguyen <penguy@microsoft.com>
2021-11-10 15:15:39 -0800

    test

--- src/lib/parser/YamlParser.ts
+++ src/lib/parser/YamlParser.ts
@@ -2,12 +2,24 @@
 import { HostParameterEntry } from "@microsoft/powerplatform-cli-wrapper/dist/host/IHostAbstractions";
 import fs = require('fs');
 import yaml = require('js-yaml');
-
+import path = require("path");
 export class YamlParser {
     public getHostParameterEntries(workingDir: string, actionFolder: string): Record<string, HostParameterEntry> {
         const parameterMap: Record<string, HostParameterEntry> = {};
         try {
             const file = workingDir + `\\${actionFolder}\\action.yml`;
+            const file1 = path.resolve(workingDir, `./${actionFolder}/action.yml`);
+            const file2 = path.resolve(workingDir, `/${actionFolder}/action.yml`);
+            if (fs.existsSync(file)) {
+                console.log("path: "+ file);
+            }
+            else if (fs.existsSync(file1)) {
+                console.log("path1: "+ file1);
+            }
+            else {
+                console.log("path2: "+ file2);
+            }
+
             const fileContents = fs.readFileSync(file, 'utf8');
             const data = yaml.load(fileContents) as any;
             for (const [name, inputInfo] of Object.entries(data.inputs)) {
