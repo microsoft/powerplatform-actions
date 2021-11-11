e5a8af5f90abb1936156a6b30a00372bab7cd7ca
Peterson Nguyen <penguy@microsoft.com>
2021-11-10 16:10:46 -0800

    Rm path resolve causing issues

--- src/lib/parser/YamlParser.ts
+++ src/lib/parser/YamlParser.ts
@@ -2,13 +2,12 @@
 import { HostParameterEntry } from "@microsoft/powerplatform-cli-wrapper/dist/host/IHostAbstractions";
 import fs = require('fs');
 import yaml = require('js-yaml');
-import path = require("path");
 
 export class YamlParser {
     public getHostParameterEntries(workingDir: string, actionFolder: string): Record<string, HostParameterEntry> {
         const parameterMap: Record<string, HostParameterEntry> = {};
         try {
-            const file = path.resolve(workingDir, `./${actionFolder}/action.yml`);
+            const file = workingDir + `/${actionFolder}/action.yml`;
             const fileContents = fs.readFileSync(file, 'utf8');
             const data = yaml.load(fileContents) as any;
             for (const [name, inputInfo] of Object.entries(data.inputs)) {
