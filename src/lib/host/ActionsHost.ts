218424224d7b33fbf8d73394b41742ffc3da7233
david@DAVID-PC <davidjen@microsoft.com>
2021-11-12 11:20:10 -0800

    normalize task input values to return undefined if not declared by user

--- src/lib/host/ActionsHost.ts
+++ src/lib/host/ActionsHost.ts
@@ -1,10 +1,12 @@
 import { HostParameterEntry, IHostAbstractions } from "@microsoft/powerplatform-cli-wrapper/dist/host/IHostAbstractions";
-import { getInput } from '@actions/core';
+import * as core from '@actions/core';
 
 export class ActionsHost implements IHostAbstractions {
   name = "GitHub-Actions";
 
   public getInput(entry: HostParameterEntry): string | undefined {
-    return getInput(entry.name, { required: entry.required });
+    const value = core.getInput(entry.name, { required: entry.required });
+    // normalize value to always be undefined if the user has not declared the input value
+    return (value && value.trim() !== '') ? value : undefined;
   }
 }
