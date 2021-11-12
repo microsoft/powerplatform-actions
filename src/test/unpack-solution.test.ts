6259877b747a63af2d14242582d0e3ffc6cd12b9
david@DAVID-PC <davidjen@microsoft.com>
2021-11-12 14:02:04 -0800

    remove auth/envUrl params from pack/unpack-solutions

--- src/test/unpack-solution.test.ts
+++ src/test/unpack-solution.test.ts
@@ -2,11 +2,9 @@
 // Licensed under the MIT License.
 
 import { should, use } from "chai";
-import { stubInterface } from "ts-sinon";
 import * as sinonChai from "sinon-chai";
 import rewiremock from "./rewiremock";
 import { fake, stub } from "sinon";
-import { UsernamePassword } from "@microsoft/powerplatform-cli-wrapper";
 import { runnerParameters } from "../../src/lib/runnerParameters";
 import Sinon = require("sinon");
 import { ActionsHost } from "../lib/host/ActionsHost";
@@ -16,16 +14,12 @@ use(sinonChai);
 describe("unpack solution test", () => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const unpackSolutionStub: Sinon.SinonStub<any[], any> = stub();
-  const credentials: UsernamePassword = stubInterface<UsernamePassword>();
-  const environmentUrl = "environment url";
 
   async function callActionWithMocks(): Promise<void> {
     await rewiremock.around(
       () => import("../../src/actions/unpack-solution/index"),
       (mock) => {
         mock(() => import("@microsoft/powerplatform-cli-wrapper/dist/actions")).with({ unpackSolution: unpackSolutionStub });
-        mock(() => import("../../src/lib/auth/getCredentials")).withDefault(() => credentials );
-        mock(() => import("../../src/lib/auth/getEnvironmentUrl")).withDefault(() => environmentUrl );
         mock(() => import("fs/promises")).with({ chmod: fake() });
         mock(() => import("../../src/lib/runnerParameters")).with({ runnerParameters: runnerParameters });
       });
@@ -36,8 +30,6 @@ describe("unpack solution test", () => {
     await callActionWithMocks();
 
     unpackSolutionStub.should.have.been.calledOnceWithExactly({
-      credentials: credentials,
-      environmentUrl: environmentUrl,
       solutionZipFile: { name: 'solution-file', required: true, defaultValue: undefined },
       sourceFolder: { name: 'solution-folder', required: true, defaultValue: undefined },
       solutionType: { name: 'solution-type', required: false, defaultValue: "Unmanaged" },
