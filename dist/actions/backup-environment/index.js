d85905a5339b8b4081da8387e37634dbb672a402
Praveen <vyelleswarap@microsoft.com>
2022-01-04 11:39:17 -0800

    Update dist

--- dist/actions/backup-environment/index.js
+++ dist/actions/backup-environment/index.js
@@ -5116,6 +5116,7 @@ function main() {
     core.startGroup("backup-environment:");
     yield actions_1.backupEnvironment({
       credentials: getCredentials_1.default(),
+      environment: parameterMap["environment"],
       environmentUrl: parameterMap["environment-url"],
       backupLabel: parameterMap["backup-label"],
       notes: parameterMap["notes"]
