d4b8e0a4921252271b23681d6261f265c2eb93df
NORTHAMERICA\sbhalgat <sbhalgat@microsoft.com>
2022-01-17 19:52:21 -0800

    add buildVersion and revisionVersion arguments

--- src/actions/update-solution-version/actionLaunch.ts
+++ src/actions/update-solution-version/actionLaunch.ts
@@ -3,7 +3,8 @@
 import process = require('process');
 import { main } from './index';
 
-process.env['INPUT_PATCH-VERSION'] = '10';
+process.env['INPUT_BUILD-VERSION'] = '1';
+process.env['INPUT_REVISION-VERSION'] = '1';
 // process.env['INPUT_STRATEGY'] = 'solution';
 
 process.env['RUNNER_DEBUG'] = '1';
