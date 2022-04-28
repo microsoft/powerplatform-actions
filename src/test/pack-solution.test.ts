d12abc02ae3f836882afe199b91558804c99e58b
Paul Breuler <4356863+paulbreuler@users.noreply.github.com>
2022-04-28 12:28:36 -0400

    Update tests to mock action/core isDebug method

--- src/test/pack-solution.test.ts
+++ src/test/pack-solution.test.ts
@@ -8,47 +8,55 @@ import { fake, stub } from "sinon";
 import { runnerParameters } from "../../src/lib/runnerParameters";
 import Sinon = require("sinon");
 import { ActionsHost } from "../lib/host/ActionsHost";
+import * as core from '@actions/core';
+
 should();
 use(sinonChai);
 
 describe("pack solution test", () => {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const packSolutionStub: Sinon.SinonStub<any[], any> = stub();
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const packSolutionStub: Sinon.SinonStub<any[], any> = stub();
 
-  async function callActionWithMocks(): Promise<void> {
-    await rewiremock.around(
-      () => import("../../src/actions/pack-solution/index"),
-      (mock) => {
-        mock(() => import("@microsoft/powerplatform-cli-wrapper/dist/actions")).with({ packSolution: packSolutionStub });
-        mock(() => import("fs/promises")).with({ chmod: fake() });
-        mock(() => import("../../src/lib/runnerParameters")).with({ runnerParameters: runnerParameters });
-      });
-  }
+    before(() => {
+        Sinon.restore();
+        Sinon.replace(core, "isDebug", () => true);
+    })
 
-  it("calls pack solution", async () => {
+    async function callActionWithMocks(): Promise<void> {
 
-    await callActionWithMocks();
+        await rewiremock.around(
+            () => import("../../src/actions/pack-solution/index"),
+            (mock) => {
+                mock(() => import("@microsoft/powerplatform-cli-wrapper/dist/actions")).with({ packSolution: packSolutionStub });
+                mock(() => import("fs/promises")).with({ chmod: fake() });
+                mock(() => import("../../src/lib/runnerParameters")).with({ runnerParameters: runnerParameters });
+            });
+    }
 
-    packSolutionStub.should.have.been.calledOnceWithExactly({
-      solutionZipFile: { name: 'solution-file', required: true, defaultValue: undefined },
-      sourceFolder: { name: 'solution-folder', required: true, defaultValue: undefined },
-      solutionType: { name: 'solution-type', required: false, defaultValue: "Unmanaged" },
-      errorLevel: { name: 'errorLevel', required: false, defaultValue: 'Info' },
-      singleComponent: { name: 'single-component', required: false, defaultValue: 'None' },
-      mapFile: { name: 'map-file', required: false, defaultValue: undefined },
-      localeTemplate: { name: 'locale-template', required: false, defaultValue: undefined },
-      localize: { name: 'localize', required: false, defaultValue: undefined },
-      useLcid: { name: 'use-lcid', required: false, defaultValue: 'false' },
-      useUnmanagedFileForManaged: {
-          name: 'use-unmanaged-file-for-missing-managed',
-          required: false,
-          defaultValue: 'false'
-      },
-      disablePluginRemap: {
-          name: 'disable-plugin-remap',
-          required: false,
-          defaultValue: 'false'
-      }
-    }, runnerParameters, new ActionsHost());
-  });
+    it("calls pack solution", async () => {
+
+        await callActionWithMocks();
+
+        packSolutionStub.should.have.been.calledOnceWithExactly({
+            solutionZipFile: { name: 'solution-file', required: true, defaultValue: undefined },
+            sourceFolder: { name: 'solution-folder', required: true, defaultValue: undefined },
+            solutionType: { name: 'solution-type', required: false, defaultValue: "Unmanaged" },
+            errorLevel: { name: 'errorLevel', required: false, defaultValue: 'Verbose' },
+            singleComponent: { name: 'single-component', required: false, defaultValue: 'None' },
+            mapFile: { name: 'map-file', required: false, defaultValue: undefined },
+            localeTemplate: { name: 'locale-template', required: false, defaultValue: undefined },
+            localize: { name: 'localize', required: false, defaultValue: undefined },
+            useLcid: { name: 'use-lcid', required: false, defaultValue: 'false' },
+            useUnmanagedFileForManaged: {
+                name: 'use-unmanaged-file-for-missing-managed',
+                required: false,
+                defaultValue: 'false'
+            },
+            disablePluginRemap: {
+                name: 'disable-plugin-remap',
+                required: false,
+                defaultValue: 'false'
+            }
+        }, runnerParameters, new ActionsHost());
+    });
 });
