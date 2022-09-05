437c15327d85290d5d9389a45cc3d377412f7a4d
Andrew Petrochuk <30735471+petrochuk@users.noreply.github.com>
2022-09-05 12:44:36 -0700

    Added data import/export actions (#241)
    * Added data import/export actions

---
+++ src/actions/export-data/index.ts
@@ -0,0 +1,39 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { dataExport } from "@microsoft/powerplatform-cli-wrapper/dist/actions";
+import * as core from '@actions/core';
+import { YamlParser } from '../../lib/parser/YamlParser';
+import { ActionsHost } from '../../lib/host/ActionsHost';
+import getCredentials from "../../lib/auth/getCredentials";
+import { runnerParameters } from '../../lib/runnerParameters';
+
+(async () => {
+    const taskParser = new YamlParser();
+    const parameterMap = taskParser.getHostParameterEntries('export-data');
+    const actionsHost = new ActionsHost();
+    const workingDir = actionsHost.getInput({ name: "working-directory", required: false });
+    if (workingDir) {
+        runnerParameters.workingDir = workingDir;
+    }
+
+    core.startGroup('export-data:');
+    await dataExport({
+        credentials: getCredentials(),
+        environment: parameterMap['environment-url'],
+        schemaFile: parameterMap['schema-file'],
+        dataFile: parameterMap['data-file'],
+        overwrite: parameterMap['overwrite'],
+        verbose: {
+            name: "verbose",
+            required: false,
+            defaultValue: false
+        }
+    }, runnerParameters, actionsHost);
+
+    core.endGroup();
+})().catch(error => {
+    const logger = runnerParameters.logger;
+    logger.error(`failed: ${error}`);
+    core.endGroup();
+});
---
+++ src/test/exportData.test.ts
@@ -0,0 +1,44 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { should, use } from "chai";
+import { stubInterface } from "ts-sinon";
+import * as sinonChai from "sinon-chai";
+import rewiremock from "./rewiremock";
+import { stub } from "sinon";
+import { UsernamePassword } from "@microsoft/powerplatform-cli-wrapper";
+import { runnerParameters } from "../../src/lib/runnerParameters";
+import Sinon = require("sinon");
+import { ActionsHost } from "../lib/host/ActionsHost";
+should();
+use(sinonChai);
+
+describe("export-data tests", () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const exportDataStub: Sinon.SinonStub<any[], any> = stub();
+    const credentials: UsernamePassword = stubInterface<UsernamePassword>();
+    const mockEnvironmentUrl = "https://contoso.crm.dynamics.com/";
+
+    async function callActionWithMocks(): Promise<void> {
+        await rewiremock.around(() => import("../../src/actions/export-data/index"),
+            (mock) => {
+                mock(() => import("@microsoft/powerplatform-cli-wrapper/dist/actions")).with({ dataExport: exportDataStub });
+                mock(() => import("../../src/lib/auth/getCredentials")).withDefault(() => credentials);
+                mock(() => import("../../src/lib/auth/getEnvironmentUrl")).withDefault(() => mockEnvironmentUrl);
+                mock(() => import("../../src/lib/runnerParameters")).with({ runnerParameters: runnerParameters });
+            });
+    }
+
+    it("fetches parameters from index.ts, calls exportDataStub properly", async () => {
+        await callActionWithMocks();
+
+        exportDataStub.should.have.been.calledWithExactly({
+            credentials: credentials,
+            environment: { name: 'environment-url', required: true, defaultValue: undefined },
+            schemaFile: { name: 'schema-file', required: true, defaultValue: undefined },
+            dataFile: { name: 'data-file', required: true, defaultValue: undefined },
+            overwrite: { name: 'overwrite', required: false, defaultValue: undefined },
+            verbose: { name: 'verbose', required: false, defaultValue: false }
+        }, runnerParameters, new ActionsHost());
+    });
+});
