e295c83c698d6015962a1c2676d52a445a82d6d3
David JENNI <3200210+davidjenni@users.noreply.github.com>
2022-11-22 16:35:20 -0800

    use a dedicated automation user for our integration tests (#260)
    - separate user with lower privileges: ppdevautomation@ppdevtools
    - password is set as repo secret: PASSWORD_PPDEVTOOLS
    - to refresh the password, use admin.powerplatform.com of our ppdevtools tenant,
        then update the repo secret & KV
    - password stored in KV: https://ms.portal.azure.com/#@microsoft.onmicrosoft.com/asset/Microsoft_Azure_KeyVault/Secret/https://toolscorebuild.vault.azure.net/secrets/ppdevautomation-on-ppdevtools
    
    Co-authored-by: davidjen@DAVIDJENDEV <davidjen@microsoft.com>

--- src/actions/import-data/actionLaunch.ts
+++ src/actions/import-data/actionLaunch.ts
@@ -5,7 +5,7 @@ import path = require('path');
 import process = require('process');
 
 process.env['INPUT_ENVIRONMENT-URL'] = new URL(process.env['PA_BT_ORG_URL'] ?? 'https://davidjenD365-1.crm.dynamics.com').href;
-process.env['INPUT_USER-NAME'] = process.env['PA_BT_ORG_USER'] ?? 'davidjen@ppdevtools.onmicrosoft.com';
+process.env['INPUT_USER-NAME'] = process.env['PA_BT_ORG_USER'] ?? 'ppdevautomation@ppdevtools.onmicrosoft.com';
 process.env['INPUT_APP-ID'] = process.env['PA_BT_ORG_SPN_ID'] ?? '8a7729e0-2b71-4919-a89a-c789d0a9720a';
 process.env['INPUT_CLIENT-SECRET'] = process.env['PA_BT_ORG_SPNKEY'] ?? "expectSpnKeyFromEnvVariable";
 process.env['INPUT_TENANT-ID'] = process.env['PA_BT_ORG_SPN_TENANT_ID'] ?? '3041a058-5110-495a-a575-b2a5571d9eac';
