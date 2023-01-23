a3a603909b0a10f208bd0595dfd3ff58d86b4577
david@DAVID-PC <davidjen@microsoft.com>
2023-01-23 09:45:10 -0800

    Dec refresh: pac CLI 1.21.8

--- gulpfile.js
+++ gulpfile.js
@@ -197,7 +197,7 @@ async function addDistToIndex() {
     console.log(`stderr: ${res.stderr}`);
 }
 
-const cliVersion = '1.21.4';
+const cliVersion = '1.21.8';
 
 async function nugetInstallPortalPackages() {
     const packageName = "CDSStarterPortal"
