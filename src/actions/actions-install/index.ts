220dd262c909de40c0227be911235c41b5b4bcfe
Crash Collison <3751389+tehcrashxor@users.noreply.github.com>
2023-08-18 14:28:18 -0700

    Better error messages and warnings (#428)
    * Add warning when overriding PAC version, and check for dotnet / nuget on runner
    
    * Better error message, and debug code cleanup

--- src/actions/actions-install/index.ts
+++ src/actions/actions-install/index.ts
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 import * as core from '@actions/core';
 import * as exec from '@actions/exec';
+import * as io from '@actions/io'
 import * as os from "node:os";
 import { dirname, resolve } from 'node:path';
 import * as fs from 'node:fs/promises';
@@ -23,6 +24,11 @@ import { PacInstalledEnvVarName } from '../../lib/runnerParameters';
 export async function main(): Promise<void> {
     core.startGroup('actions-install:');
     const versionArg = core.getInput('pac-version-override', { required: false, trimWhitespace: true });
+
+    if (versionArg && versionArg !== PacInfo.PacPackageVersion) {
+        core.warning(`Actions built targetting PAC ${PacInfo.PacPackageVersion}, so Action and PAC parameters might not match with requested version ${versionArg}.`);
+    }
+
     const packageVersion = versionArg || PacInfo.PacPackageVersion;
 
     core.info(`Installing pac ${packageVersion}...`);
@@ -53,6 +59,8 @@ async function nugetInstall(packageName: string, packageVersion: string, install
 
     const outputDirectory = dirname(installDir);
 
+    await checkForInstallationTool('nuget');
+
     await exec.getExecOutput('nuget', ['install', packageName,
         '-Version', packageVersion,
         '-DependencyVersion', 'ignore', // There are no dependencies, so don't waste that time checking
@@ -68,7 +76,17 @@ async function nugetInstall(packageName: string, packageVersion: string, install
 async function dotnetInstall(packageName: string, packageVersion: string, installDir: string): Promise<void> {
     core.info(`Installing ${packageName}.${packageVersion} via dotnet tool install`);
 
+    await checkForInstallationTool('dotnet');
+
     await exec.getExecOutput('dotnet', ['tool', 'install', packageName,
         '--version', packageVersion,
         '--tool-path', installDir]);
+}
+
+async function checkForInstallationTool(toolName: string): Promise<void> {
+    try {
+        await io.which(toolName, true);
+    } catch (error) {
+        throw new Error(`Runner does not have prerequisite "${toolName}" installed, as it was not found in the PATH.`);
+    }
 }
