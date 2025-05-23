390e600dddc0a5c9e1eb389b5765de4438449853
MattB <mattb-msft@hotmail.com>
2025-05-23 17:36:38 +0000

    Update Pac File

--- dist/actions/actions-install/index.js
+++ dist/actions/actions-install/index.js
@@ -19808,7 +19808,7 @@ var require_pacPackageInfo = __commonJS({
       PacPackageName: "Microsoft.PowerApps.CLI",
       LegacyLinuxPackage: "Microsoft.PowerApps.CLI.Core.linux-x64",
       DotnetToolName: "Microsoft.PowerApps.CLI.Tool",
-      PacPackageVersion: "1.34.4"
+      PacPackageVersion: "1.43.6"
     };
   }
 });
