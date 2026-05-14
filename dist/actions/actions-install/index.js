59c0b2abadc26618ca914c689ab9dc399da4d6bb
Jayanth Reddy Bujula <137358312+jbujula@users.noreply.github.com>
2026-05-13 18:31:27 -0700

    rebuild dist with PAC CLI 2.7.4 (#697)

--- dist/actions/actions-install/index.js
+++ dist/actions/actions-install/index.js
@@ -19808,7 +19808,7 @@ var require_pacPackageInfo = __commonJS({
       PacPackageName: "Microsoft.PowerApps.CLI",
       LegacyLinuxPackage: "Microsoft.PowerApps.CLI.Core.linux-x64",
       DotnetToolName: "Microsoft.PowerApps.CLI.Tool",
-      PacPackageVersion: "1.43.6"
+      PacPackageVersion: "2.7.4"
     };
   }
 });
